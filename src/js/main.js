41845e33a226763daaa0720d6242317b4fba8614
PetrVan <vanicekpe@gmail.com>
2023-02-26 19:20:08 +0100

    Přidání sekce Portfólio a slideru

--- src/js/main.js
+++ src/js/main.js
@@ -23,4 +23,25 @@ $(document).ready(function(){
     close.on('click', function(){
         modal.removeClass('modal_active');
     });
+
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+            ]
+    });
 });
