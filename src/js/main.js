a578dbd2fbca3f960a2bfdab04dbe311e816f6e5
PetrVan <vanicekpe@gmail.com>
2023-03-16 22:39:38 +0100

    Validace formulářů

--- src/js/main.js
+++ src/js/main.js
@@ -32,6 +32,27 @@ $(document).ready(function(){
         modal.removeClass('modal_active');
     });
 
+    // Validace formulářů
+    $('#brief-form').validate({
+      rules: {
+        username: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        username: "Uveďte Vaše jméno",
+        phone: "Uveďte Váš telefon",
+        email: {
+          required: "Potřebujeme Vaši emailouvou adresu",
+          email: "Vaše adresa musí být ve správném formátu"
+        }
+      }
+
+    });
+
     // Napojení slideru
     $('.slider').slick({
         slidesToShow: 3,
